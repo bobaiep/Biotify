4970b9351b03f0bc794be623d9de0bbd7959a976
Marius <marius.andre@epita.fr>
2021-06-25 15:55:36 +0200

    first commit

---
+++ spotifyStatus.py
@@ -0,0 +1,28 @@
+import spotipy
+import time
+import sys
+from spotipy.oauth2 import SpotifyOAuth
+
+args = sys.argv
+
+if len(args) != 2:
+    print('Usage: spotify-bio-updater twitter-username')
+
+else:
+    scope = "user-read-currently-playing"
+    username = sys.argv[1]
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    while(1):
+        result = sp.current_user_playing_track()
+        if result:
+            track = result['item']["name"]
+            album = result['item']['album']['name']
+            artist = result['item']['album']['artists'][0]['name']
+
+            pretty_str_playing = f"Currently listening : {track}, {album} by {artist}"
+
+            print(pretty_str_playing)
+
+        time.sleep(10)
