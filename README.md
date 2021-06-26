59dac091df4f21a371c128e78a8122fec01574d4
Marius <marius.andre@epita.fr>
2021-06-26 18:39:12 +0200

    Version 1.0

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # Welcome to Twitter Biotify 👋
-![Version](https://img.shields.io/badge/version-0.1-blue.svg?cacheSeconds=2592000)
+![Version](https://img.shields.io/badge/version-1-blue.svg?cacheSeconds=2592000)
 
 > A python bot designed to update your Twitter bio with the current listening track on Spotify
 
 ## Dependecies
 - [Spotipy](https://github.com/plamere/spotipy)
+- [Rauth](https://github.com/litl/rauth)
 
 ## Run
 1. Create an app on [Spotify for Developpers's Dashboard](https://developer.spotify.com/dashboard/)
-2. [Install Spotipy](https://spotipy.readthedocs.io/en/2.18.0/#installation)
+2. [Install Spotipy](https://spotipy.readthedocs.io/en/2.18.0/#installation) and [Rauth](https://github.com/litl/rauth#installation)
 3. Set the environment variables :
     - SPOTIPY_CLIENT_ID
     - SPOTIPY_CLIENT_SECRET
     - SPOTIPY_REDIRECT_URI
-4. Run:
+4. Create an app on [Twitter Developer Portal](https://developer.twitter.com/en)
+5. Set the environment variables (Twitter Consumer Keys) :
+    - TWITTER_API_KEY 
+    - TWITTER_API_SECRET
+6. Set up 3-legged OAuth and also Read and Write permissions 
+7. Run:
 ```sh
-py biotify.py twitter-username
+py biotify.py
 ```
 
 ## Author
--- biotify.py
+++ biotify.py
@@ -1,28 +1,114 @@
 import spotipy
-import time
-import sys
+from time import sleep
 from spotipy.oauth2 import SpotifyOAuth
+from rauth import OAuth1Service
+import os
 
-args = sys.argv
+class biotify:
+    def __init__(self) -> None:
+        self.spotify_api_auth = self.get_spotify_auth()
+        self.twitter_api_oauth = self.get_twitter_oauth()
+        self.default_bio = self.get_bio()
+        self.current_bio = self.default_bio
 
-if len(args) != 2:
-    print('Usage: biotify twitter-username')
+    def get_spotify_auth(self):
+        scope = "user-read-currently-playing"
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-else:
-    scope = "user-read-currently-playing"
-    username = sys.argv[1]
+        return sp
 
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    def get_twitter_oauth(self):
+        twitter = OAuth1Service(
+            name='twitter',
+            consumer_key=os.environ.get("TWITTER_API_KEY"),
+            consumer_secret=os.environ.get("TWITTER_API_SECRET"),
+            request_token_url='https://api.twitter.com/oauth/request_token',
+            access_token_url='https://api.twitter.com/oauth/access_token',
+            authorize_url='https://api.twitter.com/oauth/authorize',
+            base_url='https://api.twitter.com/1.1/')
 
-    while(1):
-        result = sp.current_user_playing_track()
-        if result:
-            track = result['item']["name"]
-            album = result['item']['album']['name']
-            artist = result['item']['artists'][0]['name']
+        request_token, request_token_secret = twitter.get_request_token(data={'oauth_callback': 'oob'})
+        authorize_url = twitter.get_authorize_url(request_token)
 
-            pretty_str_playing = f"Currently listening : {track} - {album} by {artist}"
+        print('Visit this URL in your browser: ' + authorize_url)
+        pin = input('Enter PIN from browser: ')
 
-            print(pretty_str_playing)
+        session = twitter.get_auth_session(request_token,request_token_secret,method='POST',data={'oauth_verifier': pin})
 
-        time.sleep(10)
+        return session
+
+    def get_bio(self):
+        response = self.twitter_api_oauth.post("https://api.twitter.com/1.1/account/update_profile.json",data={},params={})
+        if response.status_code != 200:
+            raise Exception(
+                "Request returned an error: {} {}".format(
+                    response.status_code, response.text
+                )
+            )
+        json_response = response.json()
+        bio = json_response["description"]
+
+        print(f"Your Bio : {bio}\n")
+
+        return bio
+
+    def set_bio(self,desc):
+        params = {'description': desc} 
+        response = self.twitter_api_oauth.post("https://api.twitter.com/1.1/account/update_profile.json",data={},params=params)
+        if response.status_code != 200:
+            raise Exception(
+                "Request returned an error: {} {}".format(
+                    response.status_code, response.text
+                )
+            )
+
+    def run(self):
+        last_track = ""
+        idle_time = 0
+        boo = 0
+        while(1):
+            result = self.spotify_api_auth.current_user_playing_track()
+            if result:
+                track = result['item']["name"]
+                #album = result['item']['album']['name']
+                artist = result['item']['artists'][0]['name']
+
+                pretty_str_playing = f"Currently listening : {track} - {artist}"
+                
+                if last_track != track and result["is_playing"]:
+                    boo = 0
+                    idle_time = 0
+                    self.current_bio = self.default_bio + "\n" + pretty_str_playing
+                    self.set_bio(self.current_bio)
+                    print(pretty_str_playing)
+                elif boo != 1 and not result["is_playing"]:
+                    if idle_time > 30:
+                        idle_time = 0
+                        last_track = ""
+                        boo = 1
+                    else:
+                        idle_time += 1
+                elif boo == 1 and idle_time != -1:
+                    print("Idlinng for more than 5min, setting the default bio back !")
+                    self.set_bio(self.default_bio)
+                    idle_time = -1
+
+                last_track = track
+            else:
+                if boo == 0 and last_track != "":
+                    boo = 2
+                    print("Idling or Error, setting the default bio back !")
+                    self.set_bio(self.default_bio)
+
+            sleep(10)
+
+if __name__ == '__main__' :
+    session = biotify()
+    try :
+        session.run()
+    except Exception as err:
+        print("[Error] {}".format(err))
+    except KeyboardInterrupt:
+        print("Setting the default bio back !")
+        print("Thank you for using biotify, see you soon !")
+        session.set_bio(session.default_bio)
